9483653dabb2e7412cce1a19a4962d332f46c7bb
ArthaWiguna <arthawiguna486@gmail.com>
2022-06-14 20:21:52 +0800

    add react icons

---
+++ src/components/CardProduct.js
@@ -0,0 +1,15 @@
+import React from "react";
+import cardImage from "../asset/card-image.png";
+
+const cardProduct = () => {
+  return (
+    <div className="shadow-md px-2 pt-2 pb-4 w-[182px]">
+      <img src={cardImage} alt="cardImage" />
+      <h3 className="mt-2 text-sm font-normal">Jam Tangan Casio</h3>
+      <p className="mt-1 text-[10px] font-normal text-gray-900">Aksesoris</p>
+      <h3 className="mt-2 text-sm font-normal">Rp 250.000</h3>
+    </div>
+  );
+};
+
+export default cardProduct;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,17 @@
 import React from "react";
+import CardProduct from "../components/CardProduct";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="w-full px-[136px] grid grid-cols-6 justify-between">
+      <CardProduct />
+      <CardProduct />
+      <CardProduct />
+      <CardProduct />
+      <CardProduct />
+      <CardProduct />
+    </div>
+  );
 };
 
 export default Home;
