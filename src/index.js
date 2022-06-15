91f2d7ca52a16900cd31118fa025254e57c9900c
ArthaWiguna <arthawiguna486@gmail.com>
2022-06-15 13:51:43 +0800

    add pagination

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "../src/app/store.js";
+import "antd/dist/antd.min.css";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,14 @@
 import React from "react";
 import Banner from "../components/Banner";
 import CardProduct from "../components/CardProduct";
-import { FiSearch } from "react-icons/fi";
+import { FiSearch, FiPlus } from "react-icons/fi";
+import { Pagination } from "antd";
 
 const Home = () => {
+  const handlePaginationChange = (page) => {
+    // mengambil value dari pagination yang active
+    console.log(page);
+  };
   return (
     <div>
       <Banner />
@@ -18,20 +23,24 @@ const Home = () => {
             <FiSearch className="text-black text-xl font-bold" />
             Hobi
           </button>
-          <button className="bg-purple-100 px-7 py-3 flex items-center gap-2 rounded-xl text-black text-sm font-normal mt-4">
-            <FiSearch className="text-black text-xl font-bold" />
-            Hobi
-          </button>
-          <button className="bg-purple-100 px-7 py-3 flex items-center gap-2 rounded-xl text-black text-sm font-normal mt-4">
-            <FiSearch className="text-black text-xl font-bold" />
-            Hobi
-          </button>
-          <button className="bg-purple-100 px-7 py-3 flex items-center gap-2 rounded-xl text-black text-sm font-normal mt-4">
-            <FiSearch className="text-black text-xl font-bold" />
-            Hobi
-          </button>
         </div>
-        <div className="grid grid-cols-6 justify-between mt-[14px]">
+        <div className="grid grid-cols-6 justify-between gap-y-3 mt-[14px]">
+          <CardProduct />
+          <CardProduct />
+          <CardProduct />
+          <CardProduct />
+          <CardProduct />
+          <CardProduct />
+          <CardProduct />
+          <CardProduct />
+          <CardProduct />
+          <CardProduct />
+          <CardProduct />
+          <CardProduct />
+          <CardProduct />
+          <CardProduct />
+          <CardProduct />
+          <CardProduct />
           <CardProduct />
           <CardProduct />
           <CardProduct />
@@ -41,7 +50,17 @@ const Home = () => {
           <CardProduct />
           <CardProduct />
         </div>
+        <Pagination
+          className="mt-7 pb-28"
+          defaultCurrent={1}
+          total={1000}
+          onChange={handlePaginationChange}
+        />
       </div>
+      <button className="bg-purple-700 px-7 py-4 flex items-center gap-2 rounded-xl text-white text-sm font-normal mt-4 mx-auto fixed bottom-7 left-[50%] -translate-x-[50%] shadow-[0_0_10px_rgba(0, 0, 0, 0.15)]">
+        <FiPlus className="text-white text-xl font-bold" />
+        Jual
+      </button>
     </div>
   );
 };
