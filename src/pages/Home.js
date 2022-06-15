6b9d5e7bbf64b45fcb1b5acf91fa5ea95446e8f7
adlighozian <adlighozian1@gmail.com>
2022-06-15 10:12:45 +0700

    template

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,9 @@
 import React from "react";
-import CardProduct from "../components/CardProduct";
 
 const Home = () => {
   return (
-    <div className="w-full px-[136px] grid grid-cols-6 justify-between">
-      <CardProduct />
-      <CardProduct />
-      <CardProduct />
-      <CardProduct />
-      <CardProduct />
-      <CardProduct />
+    <div className="home">
+      <h1>Template</h1>
     </div>
   );
 };
