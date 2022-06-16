87a69ac4a4ea717a82bee7a9cb393f19175b51b6
adlighozian <adlighozian1@gmail.com>
2022-06-16 20:17:59 +0700

    fix login

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import InfoProduk from "../pages/InfoProduk";
 import InfoProfile from "../pages/InfoProfile";
+import Login from "../pages/Login";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/infoprofile" element={<InfoProfile />} />
         <Route path="/infoproduk" element={<InfoProduk />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
