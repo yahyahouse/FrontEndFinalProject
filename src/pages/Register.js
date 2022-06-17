fb567de37fee0973fd5fe69c9dbbd1f2537a636b
adlighozian <adlighozian1@gmail.com>
2022-06-17 21:17:08 +0700

    deploy

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,16 +1,19 @@
-import React from 'react'
-import LoginImg from '../assets/img/Loginimage.png';
+import React from "react";
+import { Link } from "react-router-dom";
+import LoginImg from "../assets/img/Loginimage.png";
 
 const Register = () => {
   return (
-    <div className='grid grid-cols-1 sm:grid-cols-2 h-screen w-full'>
-        <div className='hidden sm:block bg-register bg-no-repeat'>
-            <img classname='w-full h-full object-cover' src={LoginImg} alt=''/>
-        </div>
-        <div className='flex flex-col justify-center'>
-            <form className='max-w-[450px] w-full mx-auto p-8 px-8 rounded-2xl'>
-                <h2 className='text-35px text-black text-2xl font-bold py-4'>Daftar</h2>
-                <div className="flex flex-col text-black py-2">
+    <div className="grid grid-cols-1 sm:grid-cols-2 h-screen w-full">
+      <div className="hidden sm:block bg-register bg-no-repeat">
+        <img classname="w-full h-full object-cover" src={LoginImg} alt="" />
+      </div>
+      <div className="flex flex-col justify-center">
+        <form className="max-w-[450px] w-full mx-auto p-8 px-8 rounded-2xl">
+          <h2 className="text-35px text-black text-2xl font-bold py-4">
+            Daftar
+          </h2>
+          <div className="flex flex-col text-black py-2">
             <label>Namaa</label>
             <input
               className="rounded-xl p-2 text-black"
@@ -18,7 +21,7 @@ const Register = () => {
               type="text"
             />
           </div>
-                <div className="flex flex-col text-black py-2">
+          <div className="flex flex-col text-black py-2">
             <label>Email</label>
             <input
               className="rounded-xl p-2 text-black"
@@ -39,14 +42,16 @@ const Register = () => {
           </button>
           <div>
             <p className="text-black text-center p-4">
-              Sudah punya akun?{" "}
-              <button className="text-purple-700 font-bold">
-                Masuk di sini
-              </button>
+              Sudah punya akun?
+              <Link to={"/login"}>
+                <button className="text-purple-700 font-bold">
+                  Masuk di sini
+                </button>
+              </Link>
             </p>
           </div>
-            </form>
-        </div>
+        </form>
+      </div>
     </div>
   );
 };
