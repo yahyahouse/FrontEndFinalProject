fcc190a44a84a5bcae5205973a7a21a3c7b23cad
ArthaWiguna <arthawiguna486@gmail.com>
2022-07-22 09:53:09 +0800

    clear bugs

--- src/routes/PrivateRoutes.js
+++ src/routes/PrivateRoutes.js
@@ -3,8 +3,9 @@ import { Navigate, Outlet } from "react-router-dom";
 
 const PrivateRoutes = () => {
   let auth =
+    localStorage.getItem("user") !== null &&
     localStorage.getItem("token") !== null
-      ? JSON.parse(localStorage.getItem("token"))
+      ? JSON.parse(localStorage.getItem("user"))
       : "";
   console.log(auth);
   return auth !== "" ? <Outlet /> : <Navigate to="/login" />;
